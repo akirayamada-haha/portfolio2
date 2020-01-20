0fefab88f94c0ed5fe0c382f99a158842f6de278
Evan Poe <evanpoe91@yahoo.com>
2020-01-20 17:43:23 -0600

    adjusted image scale, media query, icon sizes, styling

--- script.js
+++ script.js
@@ -8,26 +8,35 @@ function readyEventListeners() {
 
 function generateProjectHTML() {
   return `<h2>My Projects</h2>
+
+    <div class="parks-container project-container">
     <h4>Parks and Weather Search</h4>
     <a href="https://evanpoe.github.io/Parks-and-Weather/" target="_blank">Click me</a>
     <h5>to checkout a live version!</h5>
-    <img class="parks-and-weather" src="./images/ParkScreenshot.PNG"><br>
+    <img class="parks-and-weather project-image" src="./images/ParkScreenshot.PNG"><br>
     <i class="fab fa-html5"></i>
     <i class="fab fa-css3-alt"></i>
     <i class="fab fa-js"></i>
     <p class="app-description">A site that allows users to search for national parks (making calls to the NPS API) by keyword. Results listed will
     display basic information, and each provides a link to the particular park's webiste as well as a page for more information. "More Info" will 
     display just that, as well as asynchronously call to a weather API to display a forecast for that park! <p>
+    </div>
+    
+    <i class="fas fa-bookmark"></i>
+    
+    <div class="quiz-container project-container">
     <h4>Quiz App</h4>
     <a href="https://evanpoe.github.io/Quiz-App/" target="_blank">Click me</a>
     <h5>to checkout a live version!</h5>
-    <img class="quiz-app" src="./images/Quiz-App-Screenshot 2019-11-19 23.55.29.png"><br>
+    <img class="quiz-app project-image" src="./images/Quiz-App-Screenshot 2019-11-19 23.55.29.png"><br>
     <i class="fab fa-html5"></i>
     <i class="fab fa-css3-alt"></i>
     <i class="fab fa-js"></i>
     <p class="app-description">A series of multiple choice questions centered around working out. The score and question count are 
     calculated automatically, as well as relevent feedback to each answer submitted. I created this app to demonstrate my abilities with jQuery, 
     and it is for anyone interested in fitness! <p>
+    </div>
+
     <a href="https://github.com/EvanPoe?tab=repositories" target="_blank"><i class="fab fa-github"></br>Click here to checkout my GitHub repositories!</i>`
 }
 
